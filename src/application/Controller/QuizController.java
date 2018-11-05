d6498e8ce1d8866fab927ab6150e9fb485a3a49d
WhiteAsh15 <40161220+WhiteAsh15@users.noreply.github.com>
2018-11-05 19:40:27 +0800

    Push a lot of stuff
    Like a lot of stuff

--- src/application/Controller/QuizController.java
+++ src/application/Controller/QuizController.java
@@ -9,36 +9,76 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
 
-public class QuizController implements Initializable  {
-	
+public class QuizController implements Initializable {
+
 	@FXML
 	private AnchorPane rootPane;
+
+	@FXML
+	private Button btnStart;
+
+	@FXML
+	private ImageView imgHome;
 	
-  @FXML
-  private Button btnStart;
-  
-  @FXML
-  private ImageView imgHome;
+	@FXML
+	private RadioButton Easy00;
+	
+	@FXML
+	private RadioButton Medium01;
+	
+	@FXML
+	private RadioButton Hard02;
+	
+	@FXML
+	private Label QuizNum00;
+	
+	@FXML
+	private Button Exit00;
+	
+	@FXML
+	private Label Difficulty00;
+	
+	@FXML
+	private Label Question000;
+	
+	@FXML
+	private Button Previous00;
+	
+	@FXML
+	private Label Next00;
+	
+	@FXML
+	private Text Timer00;
+	
+	@FXML
+	private TextField ShortAnswer00;
 
-  @FXML
-  void cmdHome(MouseEvent event) throws IOException {
-  	AnchorPane newRoot = FXMLLoader.load(getClass().getResource("../Interface/Main.fxml"));
+	@FXML
+	void cmdHome(MouseEvent event) throws IOException {
+		AnchorPane newRoot = FXMLLoader.load(getClass().getResource("../Interface/Main.fxml"));
 		rootPane.getChildren().setAll(newRoot);
-  }
+	}
 
-  @FXML
-  void cmdStart(ActionEvent event) throws IOException {
-  	AnchorPane newRoot = FXMLLoader.load(getClass().getResource("../Interface/Main.fxml"));
+	@FXML
+	void cmdStart(ActionEvent event) throws IOException {
+		AnchorPane newRoot = FXMLLoader.load(getClass().getResource("../Interface/EasyQ1.fxml"));
 		rootPane.getChildren().setAll(newRoot);
-  }
+		if (Easy00.isSelected()) {
+			
+		}
+	}
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
