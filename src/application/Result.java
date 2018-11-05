31cba6b2f3ccb2d79c6d0561cda4dd554bc224c9
VincentLTY <vlty0421@gmail.com>
2018-11-05 16:00:56 +0800

    Added scene
    Add scene function on Result.java

--- src/application/Result.java
+++ src/application/Result.java
@@ -1,5 +1,26 @@
 package application;
 
-public class Result {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+public class Result extends Application{
+	public void start(Stage primaryStage) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("Result.fxml"));
+			Scene scene = new Scene(root);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
