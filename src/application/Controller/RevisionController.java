534f6bfc094101c5ac80da426705c362947d734b
Ji Young Ooi <ooijiyoung@gmail.com>
2018-11-05 19:52:25 +0800

    checkpoint

--- src/application/Controller/RevisionController.java
+++ src/application/Controller/RevisionController.java
@@ -13,7 +13,7 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 
-public class RevisionController  implements Initializable{
+public class RevisionController implements Initializable{
 
     @FXML
     private AnchorPane rootPane;
