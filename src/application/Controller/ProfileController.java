83267b6952bc73522c8a8c376f2087b06ffdf1d9
Ali Nazhan <alinazhan370@gmail.com>
2018-11-06 13:34:26 +0800

    Profile UI

---
+++ src/application/Controller/ProfileController.java
@@ -0,0 +1,56 @@
+package application.Controller;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+public class ProfileController implements Initializable {
+
+	  	@FXML
+	    private AnchorPane rootPane;
+
+	    @FXML
+	    private ImageView imgHome;
+	    
+	    @FXML
+	    private Label nameStudent;
+	    
+	    @FXML
+	    private Label icStudent;
+	    
+	    @FXML
+	    private Label dobStudent;
+	    
+	    @FXML
+	    private Label nameParent;
+	    
+	    @FXML
+	    private Label contactParent;
+	    
+	    @FXML
+	    private Label icParent;
+	    
+	    @FXML
+	    private Label address;
+
+	    @FXML
+	    void cmdHome(MouseEvent event) throws IOException {
+	    	AnchorPane newRoot = FXMLLoader.load(getClass().getResource("../Interface/Main.fxml"));
+	  		rootPane.getChildren().setAll(newRoot);
+	    }
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
