eed2dba9e14efa9d7cd0a9a16bcfb602427c1615
o2maniac <44362151+o2maniac@users.noreply.github.com>
2018-11-07 23:01:54 +0800

    Revision backend
    code is commented bc i have no idea if it should go there and i don't want to break anything
    also not working as intended, need to refer back to quiz

--- src/application/Controller/RevisionController.java
+++ src/application/Controller/RevisionController.java
@@ -2,7 +2,9 @@ package application.Controller;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Random;
 import java.util.ResourceBundle;
+import java.util.Scanner;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -46,3 +48,98 @@ public class RevisionController implements Initializable{
 		}
 
 }
+
+/*		Random operation = new Random();
+		Random number = new Random();
+		int score = 0;
+		int count = 0;
+		int ans;
+
+		System.out.println("How many questions? :");
+		Scanner keyboard = new Scanner(System.in);
+		int QuestionNo = keyboard.nextInt();
+
+		for (int i = 1; i <= QuestionNo; i++) {
+			int oper = operation.nextInt(1);
+			switch (oper) {
+			case 0:
+				int addFirst = number.nextInt(35);
+				int addSecond = number.nextInt(35);
+				System.out.println(i + ")" + addFirst + " + " + addSecond + "= ?");
+				count++;
+				int addTotal = addFirst + addSecond;
+				ans = 0;
+				System.out.println("Answer:");
+				ans = keyboard.nextInt();
+				if (ans == addTotal) {
+					System.out.println("That is correct!");
+					score++;
+				} else {
+					System.out.println("That is incorrect. The answer is " + addTotal);
+				}
+			case 1:
+				int subFirst = number.nextInt(35);
+				int subSecond = number.nextInt(35);
+				while (subSecond >= subFirst) {
+					subSecond = number.nextInt(35);
+				}
+				System.out.println(i + ")" + subFirst + " - " + subSecond + "= ?");
+				count++;
+				int subTotal = subFirst - subSecond;
+				ans = 0;
+				System.out.println("Answer:");
+				ans = keyboard.nextInt();
+				if (ans == subTotal) {
+					System.out.println("That is correct!");
+					score++;
+				} else {
+
+					System.out.println("That is incorrect. The answer is " + subTotal);
+				}
+
+			case 2:
+				int mulFirst = number.nextInt(35);
+				int mulSecond = number.nextInt(35);
+				System.out.println(i + ")" + mulFirst + " * " + mulSecond + "= ?");
+				count++;
+				int mulTotal = mulFirst * mulSecond;
+				ans = 0;
+				System.out.println("Answer:");
+				ans = keyboard.nextInt();
+				if (ans == mulTotal) {
+					System.out.println("That is correct!");
+					score++;
+				} else {
+
+					System.out.println("That is incorrect. The answer is " + mulTotal);
+				}
+
+			case 3:
+				int divFirst = number.nextInt(144);
+				int divSecond = number.nextInt(12);
+				while ((divSecond == 0) || (divSecond >= divFirst)) {
+					divSecond = number.nextInt(12);
+				}
+				System.out.println(i + ")" + divFirst + " / " + divSecond + "= ?");
+				count++;
+				int divTotal = divFirst / divSecond;
+				ans = 0;
+				System.out.println("Answer:");
+				ans = keyboard.nextInt();
+				if (ans == divTotal) {
+					System.out.println("That is correct!");
+					score++;
+				} else {
+
+					System.out.println("That is incorrect. The answer is " + divTotal);
+				}
+				break;
+
+			}
+			System.out.println("You got " + score + " correct answers out of " + count);
+		}
+		
+	}
+
+}
+*/
