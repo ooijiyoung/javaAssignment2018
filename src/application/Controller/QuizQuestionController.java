7baa4e15266cb007400d53ef180595b96c1d8f7a
WhiteAsh15 <40161220+WhiteAsh15@users.noreply.github.com>
2018-11-08 12:28:40 +0800

    ufheufhef
    dsdsdasd

--- src/application/Controller/QuizQuestionController.java
+++ src/application/Controller/QuizQuestionController.java
@@ -73,6 +73,8 @@ public class QuizQuestionController implements Initializable {
 		// TODO Auto-generated method stub
 		System.out.println("Quiz diffuculty is :" + Comms.getInstance().shareVar().getQuizDifficulty());
 	}
+	
+	
 
 	public void setQuizNum(int newNumQues) throws IOException {
 		numOfQuiz = newNumQues;
