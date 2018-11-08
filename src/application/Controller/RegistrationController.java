8f3de5d8df9326149ddcfea887e5c531aa6aca56
Ji Young Ooi <ooijiyoung@gmail.com>
2018-11-08 16:42:43 +0800

    fix duplcate email registration

--- src/application/Controller/RegistrationController.java
+++ src/application/Controller/RegistrationController.java
@@ -156,22 +156,28 @@ public class RegistrationController  implements Initializable{
     	
     	if(verification == true && pwdMatch==true) {
     		ParentDB prtDB = new ParentDB();
-      	prtDB.add(parentName, parentDOB, parentContact, parentAddr);
-      	int parentID = prtDB.getLastID();
+    		StudentDB stdDB = new StudentDB();
+    		int stdID = stdDB.selectStdIDFromEmail(stdEmail);
+    		if(stdID == 0) {
+    			prtDB.add(parentName, parentDOB, parentContact, parentAddr);
+        	int parentID = prtDB.getLastID();
+        	stdDB.add(studentName,studentDOB, stdEmail, password, parentID);
+        	stdDB.listAllStudentDebug();
+        	
+        	AlertBox.infoAlert("User Successfully Registered");
+        	AnchorPane newRoot;
+        	try {
+    				newRoot = FXMLLoader.load(getClass().getResource("../Interface/Login.fxml"));
+    				rootPane.getChildren().setAll(newRoot);
+    	    	} catch (Exception e) {
+    					AlertBox.exceptionAlert(e);
+    	    	}    	
+    		}else {
+    			verification = false;
+    			AlertBox.errorAlertNoHeader("E-Mail Already Registered");
+    			tfEmail.getStyleClass().add("is-invalid");
+    		}
       	
-      	StudentDB stdDB = new StudentDB();
-      	stdDB.add(studentName,studentDOB, stdEmail, password, parentID);
-      	
-      	stdDB.listAllStudentDebug();
-      	
-      	AlertBox.infoAlert("User Successfully Registered");
-      	AnchorPane newRoot;
-      	try {
-  				newRoot = FXMLLoader.load(getClass().getResource("../Interface/Login.fxml"));
-  				rootPane.getChildren().setAll(newRoot);
-  	    	} catch (Exception e) {
-  					AlertBox.exceptionAlert(e);
-  	    	}    	
     	}else {
     		String errmsg = "Please make sure all the fields are filled.";
     		if(pwdMatch == false && verification == true) {
