d860ebf01ca2a7c13943acd154fa6966e33a1a56
Ooi Ji Young <ooijiyoung@gmail.com>
2018-11-10 15:34:18 +0800

    remove useless class and make first scene to login page

--- src/application/Interface/Main.java
+++ src/application/Interface/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+			Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
 			Scene scene = new Scene(root);
 			primaryStage.setScene(scene);
 			primaryStage.setResizable(false);
